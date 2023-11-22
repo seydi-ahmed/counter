637745b26e25e560b7645b0f733b704e0576b9af
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2023-11-22 18:18:59 +0000

    AutoPush

---
+++ app.js
@@ -0,0 +1,10 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document/querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    
+});
